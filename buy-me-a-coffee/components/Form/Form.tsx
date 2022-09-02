43529b247230e8b515ec4803c6e6893b2c2c5758
impolska742 <vaibhav19bhardwaj@gmail.com>
2022-09-03 01:03:42 +0530

    Added Crypto compare api to calculate  usd to eth conversion

--- buy-me-a-coffee/components/Form/Form.tsx
+++ buy-me-a-coffee/components/Form/Form.tsx
@@ -1,11 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Form.module.css";
+import axios from "axios";
+import { useEthers } from "@usedapp/core";
 
-const ETH_VALUE = 0.0001;
+const ETH_TO_USD_URL =
+  "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,MATIC&tsyms=USD";
 
 const Form = () => {
   const [message, setMessage] = useState<string>("");
   const [name, setName] = useState<string>("");
+  const { account: userWalletAddress, active, chainId, library } = useEthers();
+
+  const [rate, setRate] = useState({
+    eth: 0,
+    matic: 0,
+  });
+
+  const init = async () => {
+    const { data } = await axios.get(ETH_TO_USD_URL);
+    setRate({
+      eth: data.ETH.USD,
+      matic: data.MATIC.USD,
+    });
+  };
+
+  useEffect(() => {
+    init();
+    return () => {
+      setRate({
+        eth: 0,
+        matic: 0,
+      });
+    };
+  }, [chainId, active]);
+
   return (
     <form className={styles.form}>
       <div className={styles.formInput}>
@@ -28,9 +56,13 @@ const Form = () => {
           placeholder="Your Message"
         />
       </div>
-      <button>Send Message for {ETH_VALUE} ETH</button>
+      <button className={styles.buyCoffeeButton}>Send Message</button>
+      <h3>
+        5$ ~{" "}
+        {5 / rate.eth ? parseFloat((5 / rate.eth).toString()).toFixed(5) : ""}{" "}
+        ETH
+      </h3>
     </form>
   );
 };
-
 export default Form;
